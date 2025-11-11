04a51d46d80d04e770c2c2a5bda07f0192917e2d
Vaishnavi <vaishnavisaai123@gmail.com>
2025-11-11 12:07:51 +0530

    first game

---
+++ snake_water_gun_game.py
@@ -0,0 +1,38 @@
+#1 for snake
+#-1 for water
+#0 for gun
+
+computer=-1 #by default
+user=input("enter your choice: ")
+userdict={
+    "s":1,
+    "w":-1,
+    "g":0
+}
+
+reversedict={
+    1:"snake",
+    -1:"water",
+    0:"gun"
+}
+
+usernum=userdict[user]
+
+print(f"you chose {reversedict[usernum]}\nand computer chose {reversedict[computer]}")
+
+if(computer==usernum):
+    print("its a draw")
+elif(computer==-1 and usernum==1):
+    print("you win")
+elif(computer==-1 and usernum==0):
+    print("you lose")
+elif(computer==1 and usernum==-1):
+    print("you lose")
+elif(computer==1 and usernum==0):
+    print("you win")
+elif(computer==0 and usernum==-1):
+    print("you win")
+elif(computer==0 and usernum==1):
+    print("you lose")
+else:
+    print("something went wrong")
